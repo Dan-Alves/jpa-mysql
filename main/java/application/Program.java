c2be1222d62ccb3fa9af83630a38733bcc89ec13
Daniel Alves <danielalves.ccomp@gmail.com>
2021-05-04 14:08:12 -0300

    create entityManager

--- main/java/application/Program.java
+++ main/java/application/Program.java
@@ -1,5 +1,9 @@
 package application;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import domain.Person;
 
 public class Program {
@@ -9,6 +13,9 @@ public class Program {
 		Person p1 = new Person(1, "Daniel", "danielalves.ccomp@gmail.com");
 		Person p2 = new Person(2, "Fulano 1", "Fulano1@gmail.com");
 		Person p3 = new Person(3, "Fulano 2", "Fulano2@gmail.com");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("example-jpa");
+		EntityManager em = emf.createEntityManager();
 		
 		System.out.println(p1);
 		System.out.println(p2);
